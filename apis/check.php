0b19840969b21ed949641687acf022864596c026
Anshuman Sandhibigraha <59245156+anshusandhi6@users.noreply.github.com>
2020-11-22 01:37:44 +0530

    Added the feature of entering their details and seeing their certificate and downloading it
    1. User can enter only their email id in the certificate_view.html.
    2. If the user is not registered then the request will not be proccessed
    3. If user has not filled up the feedback form he will be redirected back to feedback.html
    4. Once over if he enters the details he will be shown the certiifcate later which he can download.

---
+++ apis/check.php
@@ -0,0 +1,120 @@
+<?php
+
+include 'db.php';
+function validaterating($rating)
+{
+    if ($rating == "")
+    {
+        echo (json_encode(array(
+            'status' => 'failure',
+            'message' => 'Feedback'
+        )));
+        return 0;
+
+    }
+    return 1;
+}
+function validatecomments($comment_abt_event)
+{
+    if ($comment_abt_event == "")
+    {
+        echo (json_encode(array(
+            'status' => 'failure',
+            'message' => 'Feedback'
+        )));
+        return 0;
+
+    }
+    return 1;
+}
+function validatesuggestions($suggestions)
+{
+    if ($suggestions == "")
+    {
+        echo (json_encode(array(
+            'status' => 'failure',
+            'message' => 'Feedback'
+        )));
+        return 0;
+
+    }
+    return 1;
+}
+
+
+$email_id = $_POST['email_id'];
+
+
+    $query2 = mysqli_query($con, "SELECT * from user where email='".$email_id."'");
+    $count1 = mysqli_num_rows($query2);
+    if ($count1 != 0)
+    {
+        $row1 = mysqli_fetch_assoc($query2);
+        $admin_id=$row1['admin_fk'];
+        $id=$admin_id;
+        $name=$row1['user_name'];
+        $rating=$row1['rating'];
+        $comment_abt_event=$row1['comments_abt_event'];
+        $suggestions=$row1['suggestions'];
+        if(validaterating($rating) && validatesuggestions($suggestions) && validatecomments($comment_abt_event))
+        {
+        $query = mysqli_query($con, "SELECT * FROM template_preview WHERE admin_fk='".$admin_id."'");
+        if($query)
+        {
+            $row = mysqli_fetch_assoc($query);
+            $templateName=$row['template_image'];
+            $template="../assets/img/templates/".$templateName;
+            $fontSize = $row['font_size'];
+            $textAngle=0;
+            $xCoordinate=$row['x_coordinate'];
+            $yCoordinate=$row['y_coordinate'];
+            $textColor=$row['font_color'];
+            $colors= explode(",", $textColor);
+            $fontfile=$row['font_type'];
+            $fontpath = "../assets/fonts/".$fontfile.".ttf";
+            if (isset($template) && isset($fontSize) && isset($xCoordinate) &&isset($yCoordinate)&& isset($textColor) &&isset($fontpath)) {
+                $font = realpath($fontpath);
+                // echo $font;
+                $path = $templateName;
+                $ext = pathinfo($path, PATHINFO_EXTENSION);
+                if($ext=='jpg' || $ext=='jpeg')
+                {
+                    $image = imagecreatefromjpeg($template);
+                }else if($ext=='png'){
+                    $image = imagecreatefrompng($template);
+                }
+                
+                $color = imagecolorallocate($image, $colors[0], $colors[1], $colors[2]);
+                imagettftext($image, $fontSize, $textAngle, $xCoordinate, $yCoordinate, $color, $font, $name);
+                $file = $name."_".$id;
+                imagejpeg($image, "../assets/img/certificates/" . $file . ".jpg");
+                imagedestroy($image);
+                $_SESSION['certificate'] = "../assets/img/certificates/" . $file . ".jpg";
+                if(isset($_SESSION['certificate'])){
+                    echo(json_encode(array('status'=>'success','result' => $file)));
+
+                }
+            }else{
+                echo(json_encode(array('status'=>'error','result' => 'Try again later')));
+            }
+
+        }else{
+            echo(json_encode(array('status'=>'error','result' => 'Try again later')));
+        }
+    }  
+    else
+    {
+       
+    }
+    }
+    else
+    {
+        echo (json_encode(array(
+            'status' => 'failure',
+            'message' => 'You are not registered'
+        )));
+    }
+
+
+
+?>
