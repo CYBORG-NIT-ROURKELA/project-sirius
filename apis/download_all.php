88d1251007fa14ee70e56fb939b44d8a51ac936f
bislara <biswajeetsahoo54@gmail.com>
2020-12-01 20:43:13 +0530

    completed the multiple image download
    completed the multiple image download as a zip for certificate page of admin.

--- apis/download_all.php
+++ apis/download_all.php
@@ -4,6 +4,7 @@ session_start();
 $i=0;
 include 'db.php';
 $data2 = array();
+
 if ((isset($_SESSION['logged_in']) && isset($_SESSION['admin_id']))||(isset($_SESSION['user_id'])&&isset($_SESSION['name'])))
     { 
         if(isset($_SESSION['admin_id']) && isset($_SESSION['logged_in'])){
@@ -14,6 +15,8 @@ if ((isset($_SESSION['logged_in']) && isset($_SESSION['admin_id']))||(isset($_SE
             $admin_id = $_SESSION['admin_id'];
         }
        
+        $certificates = array();
+        $filenames = array();
         $query1 = mysqli_query($con, "SELECT * FROM user WHERE admin_fk='".$admin_id."'");
         
         
@@ -25,8 +28,9 @@ if ((isset($_SESSION['logged_in']) && isset($_SESSION['admin_id']))||(isset($_SE
 
             $name=$row1['user_name'];
             $data2[] = array( 
-        'user_name' => $row1['user_name']
-        );
+            'user_name' => $row1['user_name']
+            );
+
             $templateName=$row['template_image'];
             $template="../assets/img/templates/".$templateName;
             $fontSize = $row['font_size'];
@@ -52,20 +56,35 @@ if ((isset($_SESSION['logged_in']) && isset($_SESSION['admin_id']))||(isset($_SE
                 $color = imagecolorallocate($image, $colors[0], $colors[1], $colors[2]);
                 imagettftext($image, $fontSize, $textAngle, $xCoordinate, $yCoordinate, $color, $font, $name);
                 $file = $name."_".$id;
-                imagejpeg($image, "../assets/img/certificates/" . $file . ".jpg");
-                imagedestroy($image);
+
+                // imagejpeg($image, "../assets/img/certificates/" . $file . ".jpg");
+                // imagedestroy($image);
+
+                ob_start(); // Let's start output buffering.
+                imagejpeg($image); //This will normally output the image, but because of ob_start(), it won't.
+                $contents = ob_get_contents(); //Instead, output above is saved to $contents
+                ob_end_clean(); //End the output buffer.
+
+                $base64 = base64_encode($contents);
+
+                array_push($certificates,$base64);
+                array_push($filenames,$file);
+
+
                 $_SESSION['certificate'] = "../assets/img/certificates/" . $file . ".jpg";
                 if(isset($_SESSION['certificate'])){
-                    $i++;
-                   if($i==1)
-                   echo(json_encode(array('status'=>'success','result' => $admin_id))); 
+                    $i++; 
                 }
             }else{
                 echo(json_encode(array('status'=>'error','result' => 'Styling variables not given')));
-                
+                return;
             }
 
         }
+        if($i==0)
+            echo(json_encode(array('status'=>'error','result' => 'No users found.')));
+        else
+            echo(json_encode(array('status'=>'success','result' => $certificates,'filenames'=>$filenames)));
 }
 else
 {
--- assets/js/ajax/table_content.js
+++ assets/js/ajax/table_content.js
@@ -93,65 +93,47 @@ $("#download_all").click(function(event) {
         contentType: false,
         processData: false,
         success: function(response) {
-            console.log((response));
             var res = JSON.parse(response);
-            if (res.status == "success") {
+            // console.log((res.result[2]));
+
+            if (res.status == "success") 
+            {
                 swal('Successfully Generated', '', 'success');
-                var folder = "../assets/img/certificates/";
-                var x = res.result;
-                urls = [];
 
-                $.ajax({
-                    url: folder,
-                    success: function(data) {
-                        $(data).find("a").attr("href", function(i, val) {
-
-                            if ((val.match(/\.(jpe?g|png|gif)$/)) && (val.match(x))) {
-                                var res1 = folder.concat(val);
-                                urls.push(res1);
-
-                            }
-                        });
-                        
                         var nombre = "Zip_img";
                         //The function is called
-                        compressed_img(urls, nombre);
+                        compressed_img(res.result, nombre);
 
                         function compressed_img(urls, nombre) {
-                            var zip = new JSZip();
-                            var count = 0;
-                            var name = nombre + ".zip";
-                            urls.forEach(function(url) {
-                                
-                                JSZipUtils.getBinaryContent(url, function(err, data) {
-                                    if (err) {
-                                        throw err;
-                                    }
-                                    zip.file(url.split("img/")[1], data, {
-                                        binary: true
-                                    });
-                                    count++;
-                                    console.log(url.split("img/")[1]);
-                                    if (count == urls.length) {
-                                        zip.generateAsync({
-                                            type: 'blob'
-                                        }).then(function(content) {
-                                            saveAs(content, name);
 
-                                        });
-                                    }
-                                });
-                            });
+                            if(urls.length<=0)
+                            {
+                                console.log("empty user certificates");
+                                return;
+                            }
+                            var zip = new JSZip();
+                            
+                            function download(data) {
+                                const a = document.createElement("a")
+                                a.href = "data:application/zip;base64," + data
+                                a.setAttribute("download", nombre + ".zip")
+                                a.style.display = "none"
+                                a.addEventListener("click", e => e.stopPropagation()) // not relevant for modern browsers
+                                document.body.appendChild(a)
+                                setTimeout(() => { // setTimeout - not relevant for modern browsers
+                                  a.click()
+                                  document.body.removeChild(a) 
+                                }, 0)
+                              }
+
+
+                            urls.forEach((img, i) => zip.file(res.filenames[i]+".png", img, {base64: true}))
+                            zip.generateAsync({type: "base64"}).then(download)
                         }
 
 
-                    }
-                });
-
-
             } else {
-
-                swal(res.message, '', 'error');
+                swal(res.result, '', 'error');
             }
         }
     });
